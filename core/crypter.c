92ba12db0b636d6b8b2ae9cbb3f6de21744d11a5
Allan Lima <allan.lima@ilhasoft.com.br>
2019-08-11 14:03:58 -0300

    Decrypt an message

--- core/crypter.c
+++ core/crypter.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "crypter.h"
 #include "math.h"
 #include "../parse/parser.h"
 
-#define FILE_PATTERN_PUBLIC_KEY "%lld\n%lld\n"
+#define FILE_PATTERN_PUBLIC_KEY "(n, e) = (%lld, %lld)\n"
 
 typedef long long big_int;
-
-big_int encrypt_char(char c, public_key_t public_key);
 
 int generate_public_key(char *path, big_int p, big_int q, big_int e)
 {
@@ -28,7 +27,22 @@ int generate_public_key(char *path, big_int p, big_int q, big_int e)
 	}
 	fprintf(public_key, FILE_PATTERN_PUBLIC_KEY, n, e);
 	fclose(public_key);
+
 	return 1;
+}
+
+big_int encrypt_char(char c, public_key_t public_key)
+{
+	return mod_exponentiation(parse_char(c), public_key.e, public_key.n);
+}
+
+char decrypt_char(big_int c, private_key_t private_key)
+{
+	big_int m = (private_key.p - 1) * (private_key.q - 1);
+	big_int d = coprime_mod_inverse(private_key.e, m);
+	big_int n = private_key.p * private_key.q;
+
+	return unparse_char(mod_exponentiation(c, d, n));
 }
 
 int encrypt_message(char *path, char *message, public_key_t public_key)
@@ -50,8 +64,21 @@ int encrypt_message(char *path, char *message, public_key_t public_key)
 	return 1;
 }
 
-big_int encrypt_char(char c, public_key_t public_key)
+int decrypt_message(char *path, char *message, private_key_t private_key)
 {
-	printf("encrypting %c...\n", c);
-	return mod_exponentiation(parse_char(c), public_key.e, public_key.n);
+	FILE *decrypted_message = fopen(path, "w+");
+
+	if (decrypted_message == NULL) {
+		return 0;
+	}
+	char *message_char = strtok(message, " ");
+	while (message_char != NULL) {
+		big_int message_char_int = atoll(message_char);
+		fprintf(decrypted_message, "%c", decrypt_char(message_char_int, private_key));
+		message_char = strtok(NULL, " ");
+	}
+	fprintf(decrypted_message, "\n");
+	fclose(decrypted_message);
+
+	return 1;
 }
--- core/crypter.h
+++ core/crypter.h
@@ -3,14 +3,27 @@
 
 typedef long long big_int;
 typedef struct _public_key public_key_t;
+typedef struct _private_key private_key_t;
 
 struct _public_key {
 	big_int n;
 	big_int e;
 };
 
+struct _private_key {
+	big_int p;
+	big_int q;
+	big_int e;
+};
+
 int generate_public_key(char *path, big_int p, big_int q, big_int e);
 
+big_int encrypt_char(char c, public_key_t public_key);
+
+char decrypt_char(big_int c, private_key_t private_key);
+
 int encrypt_message(char *path, char *message, public_key_t public_key);
+
+int decrypt_message(char *path, char *message, private_key_t private_key);
 
 #endif

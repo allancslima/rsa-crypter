efa965cb69fbeb892f613aaa744f4df4148d0bd2
Allan Lima <allan.lima@ilhasoft.com.br>
2019-08-11 18:57:28 -0300

    Encrypt a message via CLI

--- main.c
+++ main.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include "core/crypter.h"
 
-#define PATH_PUBLIC_KEY 	   "public_key.txt"
+#define PATH_PUBLIC_KEY        "public_key.txt"
 #define PATH_ENCRYPTED_MESSAGE "encrypted.txt"
 #define PATH_DECRYPTED_MESSAGE "decrypted.txt"
 
 #define MENU_GENERATE_PUBLIC_KEY 1
-#define MENU_ENCRYPT_MESSAGE 	 2
-#define MENU_DECRYPT_MESSAGE 	 3
-#define MENU_EXIT 				 0
+#define MENU_ENCRYPT_MESSAGE     2
+#define MENU_DECRYPT_MESSAGE     3
+#define MENU_EXIT                0
 
+#define LABEL_APP                 "RSA CRYPTER\n"
 #define LABEL_GENERATE_PUBLIC_KEY "Generate public key\n"
 #define LABEL_ENCRYPT_MESSAGE 	  "Encrypt a message\n"
 #define LABEL_DECRYPT_MESSAGE 	  "Decrypt a message\n"
 #define LABEL_EXIT 	  			  "Exit\n"
 
-#define INPUT_GENERATE_PUBLIC_KEY_PRIMES  "Type a two primes P and Q: "
-#define INPUT_GENERATE_PUBLIC_KEY_EXPOENT "Type a expoent E: "
-#define INPUT_PUBLIC_KEY "Type a public key (N, E): "
+#define INPUT_GENERATE_PUBLIC_KEY_PRIMES  "Type two primes P and Q: "
+#define INPUT_GENERATE_PUBLIC_KEY_EXPOENT "Type expoent E: "
+#define INPUT_MESSAGE                     "Type a message: "
+#define INPUT_PUBLIC_KEY                  "Type the public key (N, E): "
+#define INPUT_PRIVATE_KEY                 "Type the private key (P, Q, E): "
 
 #define MSG_SUCCESS_GENERATE_PUBLIC_KEY "The public key has been generated successfully at %s\n"
 #define MSG_ERROR_GENERATE_PUBLIC_KEY   "A problem occured during the public key generation process, please check your input numbers.\n"
-#define MSG_SUCCESS_ENCRYPT_MESSAGE 	"The message has been encrypted successfully at %s\n"
-#define MSG_ERROR_ENCRYPT_MESSAGE   	"A problem occured during the encryptation process.\n"
-#define MSG_SUCCESS_DECRYPT_MESSAGE 	"The message has been decrypted successfully at %s\n"
-#define MSG_ERROR_DECRYPT_MESSAGE   	"A problem occured during the decryptation process.\n"
-#define MESSAGE_EXITED 				 	"You left from program.\n"
-#define MESSAGE_INVALID_OPTION 		 	"Choose a valid option!\n"
+#define MSG_SUCCESS_ENCRYPT_MESSAGE     "The message has been encrypted successfully at %s\n"
+#define MSG_ERROR_ENCRYPT_MESSAGE       "A problem occured during the encryptation process.\n"
+#define MSG_SUCCESS_DECRYPT_MESSAGE     "The message has been decrypted successfully at %s\n"
+#define MSG_ERROR_DECRYPT_MESSAGE       "A problem occured during the decryptation process.\n"
+#define MESSAGE_EXITED                  "You left from program.\n"
+#define MESSAGE_INVALID_OPTION          "Choose a valid option!\n"
 
 void start();
 
@@ -53,7 +56,12 @@ int main(int argc, char const *argv[])
  */
 void clear_console()
 {
-	//printf("\033[H\033[J");
+	// printf("\033[H\033[J");
+}
+
+void new_line()
+{
+	printf("\n");
 }
 
 void start()
@@ -63,15 +71,17 @@ void start()
 
 	while (1) {
 		show_menu();
-		printf("\nOption: ");
+		new_line();
+		printf("Option: ");
 		scanf("%d", &option);
-		printf("\n");
+		new_line();
 
 		if (option == MENU_EXIT) {
 			show_message(MESSAGE_EXITED);
 			break;
 		} else {
 			handle_input(option);
+			new_line();
 		}
 	}
 }
@@ -84,6 +94,8 @@ void show_message(char *message)
 void show_menu()
 {
 	clear_console();
+	printf("    %s", LABEL_APP);
+	new_line();
 	printf("%d - %s", MENU_GENERATE_PUBLIC_KEY, LABEL_GENERATE_PUBLIC_KEY);
 	printf("%d - %s", MENU_ENCRYPT_MESSAGE, LABEL_ENCRYPT_MESSAGE);
 	printf("%d - %s", MENU_DECRYPT_MESSAGE, LABEL_DECRYPT_MESSAGE);
@@ -111,16 +123,49 @@ void handle_input(int option)
 void on_generate_public_key()
 {
 	big_int p, q, e;
+	
 	printf(INPUT_GENERATE_PUBLIC_KEY_PRIMES);
 	scanf("%lld %lld", &p, &q);
+	
 	printf(INPUT_GENERATE_PUBLIC_KEY_EXPOENT);
 	scanf("%lld", &e);
-	printf("\n");
+	
+	new_line();
 
 	if (generate_public_key(PATH_PUBLIC_KEY, p, q, e)) {
 		printf(MSG_SUCCESS_GENERATE_PUBLIC_KEY, PATH_PUBLIC_KEY);
 	} else {
 		printf(MSG_ERROR_GENERATE_PUBLIC_KEY);
 	}
-	printf("\n");
+}
+
+void clear_buffer()
+{
+	char c;
+	while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void on_encrypt_message()
+{
+	clear_buffer();
+
+	char message[10000];
+	printf(INPUT_MESSAGE);
+	fgets(message, 1000, stdin);
+
+	big_int n, e;
+	printf(INPUT_PUBLIC_KEY);
+	scanf("%lld %lld", &n, &e);
+
+	public_key_t public_key;
+	public_key.n = n;
+	public_key.e = e;
+	
+	new_line();
+
+	if (encrypt_message(PATH_ENCRYPTED_MESSAGE, message, public_key)) {
+		printf(MSG_SUCCESS_ENCRYPT_MESSAGE, PATH_ENCRYPTED_MESSAGE);
+	} else {
+		printf(MSG_ERROR_ENCRYPT_MESSAGE);
+	}
 }
